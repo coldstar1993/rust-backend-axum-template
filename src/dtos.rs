1d086b8d9dc8a62bdfc99afcd5f48202436ba713
coldstar1993 <coldstar1993@163.com>
2025-05-24 23:02:44 +0800

    add login&resiter dto

---
+++ src/dtos.rs
@@ -0,0 +1,75 @@
+use crate::models::{User, UserRole};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use validator::Validate;
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize, Validate)]
+pub struct RegisterUserDto {
+    #[validate(length(min = 1, message = "Name is required"))]
+    pub name: String,
+
+    #[validate(
+        length(min = 1, message = "Email is required"),
+        email(message = "Email is invalid")
+    )]
+    pub email: String,
+
+    #[validate(
+        length(min = 1, message = "Password is required"),
+        length(min = 6, message = "Password must be at least 6 characters")
+    )]
+    pub password: String,
+
+    #[validate(
+        length(min = 1, message = "Confirm Password is required"),
+        must_match(other = "password", message = "passwords do not match")
+    )]
+    pub password_confirm: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Validate)]
+pub struct LoginUserDto {
+    #[validate(
+        length(min = 1, message = "Email is required"),
+        email(message = "Email is invalid")
+    )]
+    pub email: String,
+
+    #[validate(
+        length(min = 1, message = "Password is required"),
+        length(min = 6, message = "Password must be at least 6 characters")
+    )]
+    pub password: String,
+}
+
+#[derive(Debug, Validate, Default, Clone, Serialize, Deserialize)]
+pub struct UserPasswordUpdateDto {
+    #[validate(
+        length(min = 1, message = "New password is required."),
+        length(min = 6, message = "new password must be at least 6 characters")
+    )]
+    pub new_password: String,
+
+    #[validate(
+        length(min = 1, message = "New password confirm is required."),
+        length(
+            min = 6,
+            message = "new password confirm must be at least 6 characters"
+        ),
+        must_match(other = "new_password", message = "new passwords do not match")
+    )]
+    pub new_password_confirm: String,
+
+    #[validate(
+        length(min = 1, message = "Old password is required."),
+        length(min = 6, message = "Old password must be at least 6 characters")
+    )]
+    pub old_password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserLoginResponseDto {
+    pub status: String,
+    pub token: String,
+}
