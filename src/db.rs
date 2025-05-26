8b6551f6f5e4900047b0701b8808b9be3d3553dd
coldstar1993 <coldstar1993@163.com>
2025-05-26 13:16:55 +0800

    add fn: query user & users

--- src/db.rs
+++ src/db.rs
@@ -74,4 +74,46 @@ impl DbClient {
         Ok(())
     }
 
+    pub async fn get_user(
+        &self,
+        user_id: Option<Uuid>,
+        name: Option<&str>,
+        email: Option<&str>,
+        token: Option<&str>,
+    ) -> Result<Option<User>, sqlx::Error> {
+        let mut user: Option<User> = None;
+        if let Some(user_id) = user_id {
+            user = sqlx::query_as!(User, r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE id = $1"#,
+            user_id).fetch_optional(&self.pool).await?;
+        } else if let Some(name) = name {
+            user = sqlx::query_as!(User, r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE name = $1"#,
+            name).fetch_optional(&self.pool).await?;
+        } else if let Some(email) = email {
+            user = sqlx::query_as!(User, r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE email = $1"#,
+            email).fetch_optional(&self.pool).await?;
+        } else if let Some(token) = token {
+            user = sqlx::query_as!(User, r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE verification_token = $1"#,
+            token).fetch_optional(&self.pool).await?;
+        }
+
+        Ok(user)
+    }
+
+    pub async fn get_users(&self, page: u32, limit: u32) -> Result<Vec<User>, sqlx::Error> {
+        let offset = (page - 1) * limit;
+
+        let users = sqlx::query_as!(
+            User,
+            r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users 
+            ORDER BY created_at DESC LIMIT $1 OFFSET $2"#,
+            limit as i64,
+            offset as i64).fetch_all(&self.pool).await?;
+
+        Ok(users)
+    }
+
+    pub async fn get_user_count(&self) -> Result<i64, sqlx::Error> {
+        let cnt = sqlx::query_scalar!(r#"SELECT count(*) FROM users"#).fetch_one(&self.pool).await?;
+        Ok(cnt.unwrap_or(0))
+    }
 }
